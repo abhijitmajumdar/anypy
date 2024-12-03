b63a75b14e09d5dc01a82ac0b24cf4fdf131ae2a
Abhijit Majumdar <abhijit.g.majumdar@gmail.com>
2024-12-03 15:03:26 -0600

    arch as an option; added more version support

--- anypy.py
+++ anypy.py
@@ -5,6 +5,7 @@ from __future__ import annotations
 
 import argparse
 import pathlib
+import platform
 import shutil
 import subprocess
 import sys
@@ -22,10 +23,63 @@ ARCHITECTURES = [
 ]
 
 RELEASE_MAP = {
+    '3.8.9': '20210415',
+    '3.8.10': '20210506',
+    '3.8.11': '20210724',
+    '3.8.12': '20220227',
+    '3.8.13': '20220802',
+    '3.8.14': '20221002',
+    '3.8.15': '20221106',
+    '3.8.16': '20230726',
+    '3.8.17': '20230826',
+    '3.8.18': '20240224',
+    '3.8.19': '20240814',
+    '3.8.20': '20240909',
+    '3.9.4': '20210415',
+    '3.9.5': '20210506',
+    '3.9.6': '20210724',
+    '3.9.7': '20211017',
+    '3.9.10': '20220227',
+    '3.9.11': '20220318',
+    '3.9.12': '20220502',
+    '3.9.13': '20220802',
+    '3.9.14': '20221002',
+    '3.9.15': '20221106',
+    '3.9.16': '20230507',
+    '3.9.17': '20230726',
+    '3.9.18': '20240224',
+    '3.9.19': '20240814',
     '3.9.20': '20241016',
+    '3.10.0': '20211017',
+    '3.10.2': '20220227',
+    '3.10.3': '20220318',
+    '3.10.4': '20220528',
+    '3.10.5': '20220630',
+    '3.10.6': '20220802',
+    '3.10.7': '20221002',
+    '3.10.8': '20221106',
+    '3.10.9': '20230116',
+    '3.10.11': '20230507',
+    '3.10.12': '20230726',
+    '3.10.13': '20240224',
+    '3.10.14': '20240814',
     '3.10.15': '20241016',
+    '3.11.1': '20230116',
     '3.11.3': '20230507',
+    '3.11.4': '20230726',
+    '3.11.5': '20230826',
+    '3.11.6': '20231002',
+    '3.11.7': '20240107',
+    '3.11.8': '20240224',
+    '3.11.9': '20240814',
     '3.11.10': '20241016',
+    '3.12.0': '20231002',
+    '3.12.1': '20240107',
+    '3.12.2': '20240224',
+    '3.12.3': '20240415',
+    '3.12.4': '20240726',
+    '3.12.5': '20240814',
+    '3.12.6': '20240909',
     '3.12.7': '20241016',
     '3.13.0': '20241016',
 }
@@ -42,6 +96,8 @@ def _cached_download(url: str, output: pathlib.Path):
 
 
 def _get_link(version: str, arch: str) -> str:
+    if arch not in ARCHITECTURES:
+        raise RuntimeError(f'Unsupported architecture: {arch}')
     release = RELEASE_MAP[version]
     link = (
         'https://github.com/indygreg/python-build-standalone/releases/download/'
@@ -112,11 +168,14 @@ if __name__ == '__main__':
         '--python-only',
         action='store_true',
         help='Only download the Python standalone executable, no venv')
-    parser.add_argument('-a',
-                        '--arch',
-                        choices=ARCHITECTURES,
-                        default=ARCHITECTURES[0],
-                        help='Specify your system architecture')
+    parser.add_argument(
+        '-a',
+        '--arch',
+        choices=ARCHITECTURES,
+        default=platform.machine(),
+        help=
+        'Specify your system architecture. Uses platform.machine() if unspecified'
+    )
     args = parser.parse_args()
 
     if args.python_only:
